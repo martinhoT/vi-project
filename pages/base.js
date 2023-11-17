ff16f3bc45c9ef948ff18a5a06dbee66350a2504
Martinho Tavares <martinho.tavares@ua.pt>
2023-11-17 20:51:25 +0000

    Refactored filter setup to be reusable between pages

--- pages/base.js
+++ pages/base.js
@@ -1,4 +1,4 @@
-let currentURL = new URL(window.location);
+const currentURL = new URL(window.location);
 
 $.get("bases/nav.html", function(data) {
     $("#nav").replaceWith(data);
@@ -12,8 +12,217 @@ $.get("bases/nav.html", function(data) {
     })
 });
 
-// TODO: how do we manually script filters for each page? The HTML elements added this way aren't detected in JS
-// $.get("bases/filters.html", function(data) {
-//     $("#filters").replaceWith(data);
-// });
+// Set filters' default values and get the filters' values if passed on the URL
+// The null dates will be set when the data is obtained and extents are extracted
+// Whenever the filters change, dispatch the custom event 'data-update' with the appropriate details.
+// D3 code should listen for these events
+const filter = {
+    cityTraversal: 0,
+    clientType: 0,
+    timeStart: null,
+    timeEnd: null
+}        
 
+for (const [key, value] of currentURL.searchParams) {
+    let parsedValue = null;
+
+    // Number
+    if (["cityTraversal", "clientType"].includes(key))
+        parsedValue = +value;
+
+    // Date
+    else if (["timeStart", "timeEnd"].includes(key))
+        parsedValue = new Date(+value);
+
+    if (parsedValue != null)
+        filter[key] = parsedValue
+}
+
+$.get("bases/filters.html", function(data) {
+    $("#filters").replaceWith(data);
+
+    // Initialize the slider-3-toggle filters since they don't depend on the data
+    $("#filters").ready(function() {
+        $("#filters").show();
+
+        for (let filter of ["#filter-city-traversal", "#filter-client-type"]) {
+            for (let i = 0; i < 3; i++) {
+                $(filter).children(".slider-3-toggle").children(".slider-3-toggle-clickable-areas").children(".slider-3-toggle-clickable-area-" + i).on("click", function() {
+                    changeSlider3ToggleState(filter, i);
+                })
+            }
+        }
+    });
+});
+
+const changeSlider3ToggleState = function(filterId, state) {
+    let left_position = 5 + 35 * state;
+    // TODO: animation
+    $(filterId).children(".slider-3-toggle").children(".slider-3-toggle-circle").css("left", left_position + "px");
+
+    // Change the labels' color
+    if (state <= 1) {
+        $(filterId).children(".filter-label-left").css("color", "#0CA789");
+    }
+    else {
+        $(filterId).children(".filter-label-left").css("color", "#000000");
+    }
+
+    if (state >= 1) {
+        $(filterId).children(".filter-label-right").css("color", "#0CA789");
+    }
+    else {
+        $(filterId).children(".filter-label-right").css("color", "#000000");
+    }
+    
+    let filterElem = document.getElementById(filterId.slice(1));
+    let changed = filterElem.value != state;
+    filterElem.value = state;
+    if (changed) {
+        if (filterId == "#filter-city-traversal") {
+            filter.cityTraversal = state;
+            console.log("Filter by city traversal");
+        }
+        else if (filterId == "#filter-client-type") {
+            filter.clientType = state;
+            console.log("Filter by client type");
+        }
+
+        document.getElementById("content").dispatchEvent(new Event('data-update'));
+    }
+}
+
+// TODO: on brush single-click it disappears, let it stay?
+function addTimeSlider(initialLeft, initialRight, domain) {
+    const timeSliderWidth = 200
+    const timeSliderHeight = 20
+    const timeSliderLabelWidth = 100
+
+    let timeFilterSvg = d3.select('#filter-time').append('svg')
+        .attr('width', timeSliderWidth + timeSliderLabelWidth * 2)
+        .attr('height', timeSliderHeight)
+
+    const timeSlider = timeFilterSvg.append('g')
+
+    let x = d3.scaleLinear()
+        .domain(domain)
+        .range([0, timeSliderWidth])
+
+    timeSlider.selectAll('text')
+        .data([[0, 15], [timeSliderWidth + timeSliderLabelWidth, 15]])
+        .join('text')
+            .text('A')
+            .attr('x', (d) => d[0])
+            .attr('y', (d) => d[1])
+
+    var brush = d3.brushX()
+        .extent([[0, 0], [timeSliderWidth, timeSliderHeight]])
+        .on('brush', brushed)
+        .on('end', brushEnded)
+
+    var brushGroup = timeFilterSvg.append('g')
+        .attr('class', 'brush')
+        .attr('transform', 'translate(' + timeSliderLabelWidth + ',' + 0 + ')')
+        .call(brush)
+    
+    function brushed(event) {
+        // Alternative: let selection = event.selection
+        let selection = d3.brushSelection(this)
+        if (selection == null)
+            return
+
+        let range = selection.map(x.invert)
+        
+        timeSlider.selectAll("text")
+            .text(function(d, i) {
+                let date = new Date(range[i]);
+                return `${date.getUTCDate()}/${date.getUTCMonth() + 1}/${date.getUTCFullYear()}`
+            })
+        
+        // NOTE: comment if update every brush action is too heavy, and use brushEnded instead
+        // callback(range[0], range[1])
+    }
+
+    function brushEnded() {
+        let selection = d3.brushSelection(this)
+        if (selection == null)
+            return
+
+        let range = selection.map(x.invert)
+
+        if (filter.timeStart.getTime() == range[0] && filter.timeEnd.getTime() == range[1])
+            return;
+
+        filter.timeStart = new Date(range[0]);
+        filter.timeEnd = new Date(range[1]);
+        console.log("Filter by time");
+
+        document.getElementById("content").dispatchEvent(new Event('data-update'));
+    }
+
+    brush.move(brushGroup, [initialLeft, initialRight].map(x));
+
+    return brush
+}
+
+// Setup the 3 filters present in most visualizations, requiring metadata and dependent lists.
+// The metadata is used to set the initial values of the filters.
+// The dependent lists are used to highlight the graphs that are affected by the filter.
+// This function should be manually called
+function setupFilters(metadata,
+        filter_city_traversal_dependents = [],
+        filter_client_type_dependents = [],
+        filter_time_dependents = [],
+    ) {
+    const dateParser = d3.utcParse("%Y-%-m-%-d");
+    let dateExtent = [dateParser(metadata["date_min"]), dateParser(metadata["date_max"])];
+    
+    if (filter.timeStart == null)
+        filter.timeStart = dateExtent[0];
+    if (filter.timeEnd == null)
+        filter.timeEnd = dateExtent[1];
+
+    // Highlight graphs that are affected by the filter
+    function highlight_start(elements) {
+        for (let element of elements) {
+            d3.select(element)
+                .transition()
+                .duration(500)
+                .style("box-shadow", "0px 0px 25px var(--filter-accent)");
+        }
+    }
+    function highlight_stop(elements) {
+        for (let element of elements) {
+            d3.select(element)
+                .transition()
+                .duration(500)
+                .style("box-shadow", "0px 0px 0px var(--filter-accent)");
+        }
+    }
+    d3.select("#filter-city-traversal").on("mouseenter", function() {
+        highlight_start(filter_city_traversal_dependents);
+    });
+    d3.select("#filter-city-traversal").on("mouseleave", function() {
+        highlight_stop(filter_city_traversal_dependents);
+    });
+    d3.select("#filter-client-type").on("mouseenter", function() {
+        highlight_start(filter_client_type_dependents);
+    });
+    d3.select("#filter-client-type").on("mouseleave", function() {
+        highlight_stop(filter_client_type_dependents);
+    });
+    d3.select("#filter-time").on("mouseenter", function() {
+        highlight_start(filter_time_dependents);
+    });
+    d3.select("#filter-time").on("mouseleave", function() {
+        highlight_stop(filter_time_dependents);
+    });
+
+    console.log("Preparing filters...");
+    // Initial filter state
+    changeSlider3ToggleState("#filter-city-traversal", filter.cityTraversal)
+    changeSlider3ToggleState("#filter-client-type", filter.clientType)
+
+    let timeSliderBrush = addTimeSlider(filter.timeStart, filter.timeEnd, dateExtent);
+    console.log("Filters prepared");
+}
